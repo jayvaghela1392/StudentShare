2f8f18af8876be0018ec5e7ad38834348a5d2939
incogniito <skandal890@hotmail.com>
2016-08-14 22:13:16 +0100

    Add structure

---
+++ app/src/main/java/com/example/jayvaghela/loginregister/app/src/main/java/com/example/jayvaghela/loginregister/UI/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.jayvaghela.loginregister.app.src.main.java.com.example.jayvaghela.loginregister.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+
+    }
+}
