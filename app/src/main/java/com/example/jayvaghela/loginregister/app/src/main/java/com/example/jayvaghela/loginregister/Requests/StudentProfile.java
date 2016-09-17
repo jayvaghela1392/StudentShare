fa3d83022d08e2e4660f6012c793d8d97ac3ff41
incogniito <skandal890@hotmail.com>
2016-09-17 23:18:59 +0100

    new updates

--- app/src/main/java/com/example/jayvaghela/loginregister/app/src/main/java/com/example/jayvaghela/loginregister/Requests/StudentProfile.java
+++ app/src/main/java/com/example/jayvaghela/loginregister/app/src/main/java/com/example/jayvaghela/loginregister/Requests/StudentProfile.java
@@ -1,7 +1,5 @@
 package com.example.jayvaghela.loginregister.app.src.main.java.com.example.jayvaghela.loginregister.Requests;
 
-/**
- * Created by samsonaiyegbusi on 14/08/16.
- */
+
 public class StudentProfile {
 }
