17874e672af33c37779506e20446f74e84b2dd31
incogniito <skandal890@hotmail.com>
2016-09-18 18:50:04 +0100

    new updates

---
+++ app/src/main/java/com/example/jayvaghela/loginregister/app/src/main/java/com/example/jayvaghela/loginregister/UI/SeeConnections.java
@@ -0,0 +1,16 @@
+package com.example.jayvaghela.loginregister.app.src.main.java.com.example.jayvaghela.loginregister.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jayvaghela.loginregister.R;
+
+public class SeeConnections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_connections);
+    }
+
+}
