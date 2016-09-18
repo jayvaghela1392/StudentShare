b82eba8962278d59868fa3e8a3be7adc6c27cac9
incogniito <skandal890@hotmail.com>
2016-09-18 14:34:19 +0100

    new updates

---
+++ app/src/main/java/com/example/jayvaghela/loginregister/app/src/main/java/com/example/jayvaghela/loginregister/UI/SearchProfile.java
@@ -0,0 +1,13 @@
+package com.example.jayvaghela.loginregister.app.src.main.java.com.example.jayvaghela.loginregister.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_profile);
+    }
+}
