9912bf4469d33c00055a58c23c0b78ded9f7508e
incogniito <skandal890@hotmail.com>
2016-09-19 02:03:55 +0100

    Add new package

--- app/src/main/java/com/example/jayvaghela/loginregister/app/src/androidTest/java/com/example/jayvaghela/loginregister/ApplicationTest.java
+++ app/src/main/java/com/example/jayvaghela/loginregister/app/src/androidTest/java/com/example/jayvaghela/loginregister/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.jayvaghela.loginregister;
+package com.example.jayvaghela.loginregister.app.src.androidTest.java.com.example.jayvaghela.loginregister;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
